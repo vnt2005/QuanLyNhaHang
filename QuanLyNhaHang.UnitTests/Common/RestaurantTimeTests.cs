42e018d18e1534c85aa7f97d5367a65dd39f7c33
vnt2005 <vonguyenthanh2005@gmail.com>
2026-08-04 00:16:31 +0700

    Test Vietnam timezone boundaries

---
+++ QuanLyNhaHang.UnitTests/Common/RestaurantTimeTests.cs
@@ -0,0 +1,57 @@
+using QuanLyNhaHang.Application.Common.Time;
+
+namespace QuanLyNhaHang.UnitTests.Common;
+
+public class RestaurantTimeTests
+{
+    [Fact]
+    public void GetUtcRange_ConvertsVietnamDayToUtcBoundaries()
+    {
+        var localDate = new DateTime(2026, 8, 4);
+
+        var range = RestaurantTime.GetUtcRange(localDate, localDate);
+
+        Assert.Equal(
+            new DateTime(2026, 8, 3, 17, 0, 0, DateTimeKind.Utc),
+            range.StartUtc);
+        Assert.Equal(
+            new DateTime(2026, 8, 4, 17, 0, 0, DateTimeKind.Utc),
+            range.EndUtc);
+    }
+
+    [Fact]
+    public void ToLocal_AssignsEarlyUtcHoursToTheCorrectVietnamDay()
+    {
+        var utcValue = new DateTime(
+            2026,
+            8,
+            3,
+            17,
+            30,
+            0,
+            DateTimeKind.Utc);
+
+        var localValue = RestaurantTime.ToLocal(utcValue);
+
+        Assert.Equal(new DateTime(2026, 8, 4, 0, 30, 0), localValue);
+    }
+
+    [Fact]
+    public void ToUtc_TreatsUnspecifiedInputAsVietnamLocalTime()
+    {
+        var localValue = new DateTime(
+            2026,
+            8,
+            4,
+            19,
+            15,
+            0,
+            DateTimeKind.Unspecified);
+
+        var utcValue = RestaurantTime.ToUtc(localValue);
+
+        Assert.Equal(
+            new DateTime(2026, 8, 4, 12, 15, 0, DateTimeKind.Utc),
+            utcValue);
+    }
+}
