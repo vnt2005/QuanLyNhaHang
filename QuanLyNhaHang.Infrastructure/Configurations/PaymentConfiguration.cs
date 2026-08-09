53f51e7f74bc36d7fc327bebc5768ecc3b46f15b
vnt2005 <vonguyenthanh2005@gmail.com>
2026-08-09 22:57:17 +0700

    chore: ignore derived payment service charge

--- QuanLyNhaHang.Infrastructure/Configurations/PaymentConfiguration.cs
+++ QuanLyNhaHang.Infrastructure/Configurations/PaymentConfiguration.cs
@@ -12,6 +12,8 @@ public class PaymentConfiguration : IEntityTypeConfiguration<Payment>
 
         builder.HasKey(x => x.Id);
 
+        builder.Ignore(x => x.ServiceChargeAmount);
+
         builder.Property(x => x.OrderId)
             .IsRequired();
 
