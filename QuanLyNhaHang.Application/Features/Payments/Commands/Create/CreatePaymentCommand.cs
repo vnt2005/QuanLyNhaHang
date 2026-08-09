89aa34db07a9ddca78f922b6fea657a8315c5311
vnt2005 <vonguyenthanh2005@gmail.com>
2026-08-09 22:57:27 +0700

    feat: accept payment service charge

--- QuanLyNhaHang.Application/Features/Payments/Commands/Create/CreatePaymentCommand.cs
+++ QuanLyNhaHang.Application/Features/Payments/Commands/Create/CreatePaymentCommand.cs
@@ -9,6 +9,8 @@ public class CreatePaymentCommand : IRequest<PaymentDto>
 
     public decimal DiscountAmount { get; set; }
 
+    public decimal ServiceChargeAmount { get; set; }
+
     public decimal VatAmount { get; set; }
 
     public decimal CustomerPaid { get; set; }
