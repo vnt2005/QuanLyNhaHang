9f1b31c2e7aa3902614f2fe502d1c3ce8881f398
vnt2005 <vonguyenthanh2005@gmail.com>
2026-08-27 17:26:50 +0700

    fix: nạp icon ngừng danh mục cuối cùng

--- QuanLyNhaHang.Frontend/src/main.tsx
+++ QuanLyNhaHang.Frontend/src/main.tsx
@@ -66,6 +66,7 @@ import './design-system/action-icons.css'
 import './design-system/action-icons-final-fix.css'
 import './design-system/action-icons-reference-standard.css'
 import './design-system/action-icons-center-fix.css'
+import './design-system/menu-category-stop-icon.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
