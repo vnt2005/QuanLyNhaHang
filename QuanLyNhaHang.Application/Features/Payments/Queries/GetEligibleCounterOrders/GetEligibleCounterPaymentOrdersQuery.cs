8715ea0f9994ea7bb903420778e2766fdd54c610
vnt2005 <vonguyenthanh2005@gmail.com>
2026-09-03 23:32:50 +0700

    refactor(payment): remove counter payment eligibility query

--- QuanLyNhaHang.Application/Features/Payments/Queries/GetEligibleCounterOrders/GetEligibleCounterPaymentOrdersQuery.cs
+++
@@ -1,7 +0,0 @@
-using MediatR;
-using QuanLyNhaHang.Application.Features.Payments.DTOs;
-
-namespace QuanLyNhaHang.Application.Features.Payments.Queries.GetEligibleCounterOrders;
-
-public sealed record GetEligibleCounterPaymentOrdersQuery
-    : IRequest<IReadOnlyList<EligibleCounterPaymentOrderDto>>;
