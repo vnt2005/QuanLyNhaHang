463878644a7d513547a0082417e116fed9893e33
vnt2005 <vonguyenthanh2005@gmail.com>
2026-09-06 22:37:40 +0700

    fix(payments): apply verified SePay policy to list

--- QuanLyNhaHang.Application/Features/Payments/Queries/GetList/GetPaymentsQueryHandler.cs
+++ QuanLyNhaHang.Application/Features/Payments/Queries/GetList/GetPaymentsQueryHandler.cs
@@ -1,8 +1,8 @@
 ﻿using MediatR;
 using Microsoft.EntityFrameworkCore;
 using QuanLyNhaHang.Application.Common.Interfaces;
+using QuanLyNhaHang.Application.Common.Payments;
 using QuanLyNhaHang.Application.Features.Payments.DTOs;
-using QuanLyNhaHang.Domain.Entities;
 
 namespace QuanLyNhaHang.Application.Features.Payments.Queries.GetList;
 
@@ -20,15 +20,9 @@ public class GetPaymentsQueryHandler
         GetPaymentsQuery request,
         CancellationToken cancellationToken)
     {
-        var payments = await _context.Payments
-            .AsNoTracking()
-            .Where(payment =>
-                payment.Status == "Paid" &&
-                payment.PaymentMethod == "BankTransfer" &&
-                _context.PaymentAttempts.Any(attempt =>
-                    attempt.PaymentId == payment.Id &&
-                    attempt.Provider == "SePay" &&
-                    attempt.Status == PaymentAttempt.PaidStatus))
+        var payments = await VerifiedSePayPaymentPolicy.Apply(
+                _context.Payments.AsNoTracking(),
+                _context)
             .OrderByDescending(x => x.PaidAt)
             .Select(x => new PaymentDto
             {
